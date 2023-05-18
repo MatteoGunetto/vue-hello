b0376414327880d5a3e3acf22d464f2cacdb732c
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-18 15:07:17 +0200

    fine esercizio

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            img: 'https://images.unsplash.com/photo-1519664824562-b4bc73f9795a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80'
+
+            
+        }
+    }
+}).mount('#app');
